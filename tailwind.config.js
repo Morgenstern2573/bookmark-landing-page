0b9e3d47575c62402aeb24191d4c79d9d3e47059
Bosun Akinyemi <azurnwa.p@gmail.com>
2020-08-11 15:16:58 +0100

    Version 1 complete

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      height: {
+        fit: "fit-content",
+      },
+
+      zIndex: {
+        "-1": "-1",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
